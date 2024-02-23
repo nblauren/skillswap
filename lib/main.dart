69e4302c6005f8bb26c73a77fdb10a0fefebbe0f
Nikko Laurenciana <nblaurenciana@mediclinic.com.au>
2024-02-23 10:53:04 +0100

    Added Deep Linking

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,30 @@ import 'package:path_provider/path_provider.dart';
 import 'package:skill_swap/firebase_options.dart';
 import 'package:skill_swap/services/locator_service.dart';
 import 'package:skill_swap/ui/screens/login_screen.dart';
-
 import 'config/theme.dart';
 import 'cubit/theme_cubit.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:flutter/services.dart';
 import 'ui/screens/skeleton_screen.dart';
+
+// Handle incoming app links
+void initUniLinks() async {
+  try {
+    // Listen for incoming app links
+    final initialUri = await getInitialUri();
+    if (initialUri != null) {
+      handleAppLink(initialUri);
+    }
+  } on PlatformException {
+    // Handle exception
+    print('Failed to initialize uni_links');
+  }
+}
+
+// Handle the app link
+void handleAppLink(Uri uri) {
+  print('Received app link: $uri');
+}
 
 /// The main entry point of the application.
 void main() async {
@@ -33,6 +53,9 @@ void main() async {
   if (Platform.isAndroid) {
     await FlutterDisplayMode.setHighRefreshRate();
   }
+
+  // Initialize Deeplink
+  initUniLinks();
 
   // Initialize Hive
   final Directory tmpDir = await getTemporaryDirectory();
